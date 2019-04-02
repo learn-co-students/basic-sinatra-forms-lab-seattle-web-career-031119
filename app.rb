3ccf31be55ad68298f0383c9ad24665b5bd85ce1
github username <github email address>
2019-04-02 11:51:52 -0700

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,19 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+    erb :newteam
+  end
 
+  post '/team' do
+    @pg = params["pg"]
+    @sg = params["sg"]
+    @pf = params["pf"]
+    @sf = params["sf"]
+    @c = params["c"]
+    @coach = params["coach"]
+    @name = params["name"]
+
+    erb :team
+  end
 end
